844c03db2a779c1e4045d3832d7a7e9a884d85b1
phil <philjke@mail.ru>
2023-03-31 17:04:31 +0700

    first commit

---
+++ src/js/files/script.js
@@ -0,0 +1,46 @@
+// Подключение функционала "Чертогов Фрилансера"
+import { isMobile } from './functions.js';
+// Подключение списка активных модулей
+import { flsModules } from './modules.js';
+
+import { gsap } from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger.js';
+gsap.registerPlugin(ScrollTrigger);
+
+const tl = gsap.timeline({
+	scrollTrigger: {
+		start: 200,
+		toggleActions: 'restart reset restart reset',
+	},
+});
+document.addEventListener('DOMContentLoaded', () => {
+	gsap.to('.hero__pizza', {
+		ease: 'none',
+		rotation: 360,
+		duration: 120,
+		repeat: -1,
+	});
+	gsap.to('.hero__robot-hand', {
+		ease: 'back.out(1.7)',
+		x: 20,
+		y: -20,
+		rotation: 5,
+		duration: 15,
+	});
+	gsap.to('.hero__human-hand', {
+		ease: 'back.out(1.7)',
+		x: -20,
+		y: 20,
+		rotation: -5,
+		duration: 15,
+	});
+	tl.to('.howto__text', {
+		x: -200,
+		opacity: 1,
+	});
+
+	tl.to('.howto__pizza', {
+		x: 200,
+		opacity: 1,
+	});
+});
